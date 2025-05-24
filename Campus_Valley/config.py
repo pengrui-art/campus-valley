1caeb42dba5358699a45d98ae15a85c05f8effb7
wangdaowei2050 <2695071411@qq.com>
2025-05-24 21:53:32 +0800

    Add files via upload

---
+++ Campus_Valley/config.py
@@ -0,0 +1,12 @@
+##图像理解
+img_udstand_appId = '874b2597'
+img_udstand_apiKey = 'ec8c63f9521c4833d7cd26420a174be6'
+img_udstand_apiSecret = 'YWFhOTVhMmUwM2EwNDBjNThmZTcyM2Fl'
+
+##文生图
+RETRO_API_KEY = "rdpk-cbb03f9a693d55baeed123c1a708d681"
+
+SPARK_API_KEY = 'ec8c63f9521c4833d7cd26420a174be6'
+SPARK_API_URL = 'wss://spark-api.xf-yun.com/v4.0/chat'      #'https://spark-api-open.xf-yun.com/v1'
+SPARK_API_SECRET = 'YWFhOTVhMmUwM2EwNDBjNThmZTcyM2Fl'
+SPARK_APPID = '874b2597'
